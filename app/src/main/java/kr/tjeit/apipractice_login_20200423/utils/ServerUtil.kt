fb653fa923102312bb44aaa347bc81346a9896a1
cho881020 <kj_cho@idealidea.co.kr>
2020-04-27 22:07:17 +0900

    회원가입 요청 기능 추가

--- app/src/main/java/kr/tjeit/apipractice_login_20200423/utils/ServerUtil.kt
+++ app/src/main/java/kr/tjeit/apipractice_login_20200423/utils/ServerUtil.kt
@@ -70,7 +70,61 @@ class ServerUtil {
 
         }
 
-    }
 
+//     회원가입 기능 별도로 추가 => 액티비티에서 사용
+
+        fun putRequestSignUp(context: Context, id:String, pw:String, name:String, phone:String, handler:JsonResponseHandler?) {
+
+            val client = OkHttpClient()
+//            어떤 기능을 수행하러 가는지 주소 완성.
+//            http://192.168.0.243:5000/auth
+            val urlStr = "${BASE_URL}/auth"
+
+//            서버에 들고갈 데이터를 첨부. => POST메쏘드의 예제
+            val formBody = FormBody.Builder()
+                .add("login_id", id)
+                .add("password", pw)
+                .add("name", name)
+                .add("phone", phone)
+                .build()
+
+
+//            화면이동으로 따지면 Intent 객체를 만드는 행위.
+            val request = Request.Builder()
+                .url(urlStr)
+                .put(formBody)
+//                .header()  => API가 헤더를 요구하면 추가해야함.
+                .build()
+
+//            startActivity 처럼 실제로 요청을 날리는 코드.
+            client.newCall(request).enqueue(object : Callback {
+
+                override fun onFailure(call: Call, e: IOException) {
+//                    서버 연결 자체를 실패.
+
+//                    연결에 실패한 경위를 로그로 출력
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+//                    서버의 응답은 보통 JSON양식으로 가공되어 옴.
+//                    받을떄는 일단 String타입으로 받게됨. => JSON으로 변환해서 액티비티에 전달.
+
+                    val body = response.body!!.string()
+                    val json = JSONObject(body)
+
+                    handler?.onResponse(json)
+
+
+                }
+
+            })
+
+
+        }
+
+
+    }
 
 }
